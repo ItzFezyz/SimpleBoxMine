cdbc765fcc654091a2ba9e7de1dd4e8d95521114
ItzFezyz <Jacobcasteel@chester139.com>
2025-03-03 08:40:33 -0600

    Update Main.java

--- src/SimpleBoxMine/Main.java
+++ src/SimpleBoxMine/Main.java
@@ -1,1 +1,22 @@
+package dev.SimpleBoxMine;
 
+public final class Main extends JavaPlugin implements Listener {
+  /* ************************** *\
+  /           Credits            \
+  /   Creator of SimpleBoxMine   \
+  /           ItzFezy            \
+  */ **************************  \*
+
+  
+  // on Server Start
+  @Override
+  public void onEnable(){
+    
+  }
+  // on Server Close
+  @Override
+  public void onDisable(){
+    
+  }
+  
+}
